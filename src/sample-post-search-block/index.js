d03aa6f9a9915a989c17b1576af25542508ac710
Matthew Haines-Young <matthew@humanmade.com>
2026-06-08 17:27:24 +0100

    Fix import casing for Edit component

--- src/sample-post-search-block/index.js
+++ src/sample-post-search-block/index.js
@@ -1,13 +1,13 @@
 import { registerBlockType } from '@wordpress/blocks';
 
 import './style.scss';
-import Edit from './edit';
+import edit from './edit';
 import save from './save';
 import metadata from './block.json';
 
 registerBlockType( metadata.name, {
-	edit: Edit,
+	edit,
 	save,
 } );
 
-alert( 'test' );
+
